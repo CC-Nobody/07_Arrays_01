1af1e6cf22aaaba00a71819213778dd5533bcdd1
CC-Nobody <cc.adiermann@gmail.com>
2021-08-03 13:36:38 +0200

    Basic

--- script_01.js
+++ script_01.js
@@ -1,2 +1,69 @@
 /* Die Satzbau-Maschine | Arrays */
 
+/***** 01. Funktionalität mit Einzelparametern *****/
+// --> "Ich bin Max Mütze."
+
+/*  Teorie: Array*/
+
+// let arr;
+
+// arr = new Array(); // Konstruktor
+// arr = [] // Literal Schreibweise
+// arr = [2,4,7,12,103];
+// arr = [true,false];
+// arr = ["Ich","bin","Max","Mütze"]
+// ausgabe(arr);
+// ausgabe(arr.length);
+// ausgabe(arr[0]); // Index, bgeinnt IMMER mit 0
+// ausgabe(arr[arr.length-1]);
+
+/*  Teorie: Schleifen (for-schleife) */
+
+// for (let i = 0; i < 10; i++) {
+//     ausgabe(i)
+// }
+
+/* For-Schleife für Array-Index (Iteration) */
+let arr = ["Ich","bin","Jesus","Christus","ihr","Lappen"];
+for (let i = 0; i < arr.length; i++) {
+    ausgabe(arr[i]);
+}
+
+/******  02a Funktionalität mit Arrays 1 *****/
+// Überlegung | Kritik
+// word1, word2, word3 ... word100 :: semantschiche Struktur
+// arr[0], arr[1], arr[2], arr[3] ... arr[n] :: numerische Struktur
+// --> Transformation semantisches Problem --> numerisches Problem
+
+// ausgabe(getSentenceArr(["Ich", "bin", "Max", "Mütze"]));
+function getSentenceArr(arr) {
+    const gap = " ";
+    const dot = "."
+    const str = arr[0] + gap +
+                arr[1] + gap +
+                arr[2] + gap +
+                arr[3] +
+                dot;
+    return str;
+}
+
+
+
+// Modul: Satz-String zusammensetzen
+// ausgabe(getSentence("Ich", "bin", "Max", "Mütze"));
+function getSentence(word1,word2,word3,word4) {
+    const gap = " ";
+    const dot = "."
+    const str = word1 + gap +
+                word2 + gap +
+                word3 + gap +
+                word4 +
+                dot;
+    return str;
+}
+
+// Modul: Ausgabe in Konsole
+// ausgabe("hi"); 
+function ausgabe(outputStr) {
+    console.log(outputStr);
+}
